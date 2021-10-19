8dcd6653013606ec9bc3471302a52ec2dfe59b55
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-10-19 19:38:55 +0530

    Feat - added Image Loader component

---
+++ src/Components/UI/ImageLoader/ImageLoader.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ImageLoader = styled.img`
+  position: absolute;
+  z-index: 999;
+  top: 45%;
+  left: 45%;
+  width: 4.5rem;
+  height: 4.5rem;
+`;
+export default ImageLoader;
